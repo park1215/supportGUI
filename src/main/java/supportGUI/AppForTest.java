8dfdf225c3ef31daccc01f8a1d22b5d5eb09878b
spark <spark@LP-SPARK-W10.hq.corp.viasat.com>
2018-05-01 13:03:06 -0600

    2nd commit

--- src/main/java/supportGUI/AppForTest.java
+++ src/main/java/supportGUI/AppForTest.java
@@ -3,10 +3,13 @@ package supportGUI;
 public class AppForTest {
 
 	public Boolean returnTrue(){
+		System.out.println("returned true");
 		return true;
+		
 	}
 	
 	public Boolean returnFalse(){
+		System.out.println("returned false");
 		return false;
 	}
 }
--- src/test/java/testCases/NewConnect.java
+++ src/test/java/testCases/NewConnect.java
@@ -5,35 +5,39 @@ import static org.testng.Assert.assertTrue;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import supportGUI.AppForTest;
+
 public class NewConnect {
 
+	AppForTest aft = new AppForTest();
+	
 	@Test(priority=1)
 	public void J1170118(){
-		Assert.assertTrue(true);
+		Assert.assertTrue(aft.returnTrue());
 	}
 	
 	@Test(priority=2)
 	public void J1170119(){
-		Assert.assertTrue(true);
+		Assert.assertTrue(aft.returnTrue());
 	}
 	
 	@Test(priority=3)
 	public void J11701110(){
-		Assert.assertTrue(true);
+		Assert.assertTrue(aft.returnTrue());
 	}
 	
 	@Test(priority=4)
 	public void J11701111(){
-		Assert.assertTrue(true);
+		Assert.assertTrue(aft.returnTrue());
 	}
 	
 	@Test(priority=5)
 	public void J11701112(){
-		Assert.assertTrue(true);
+		Assert.assertTrue(aft.returnTrue());
 	}
 	
 	@Test(priority=6)
 	public void J11701113(){
-		Assert.assertTrue(true);
+		Assert.assertTrue(aft.returnTrue());
 	}
 }
