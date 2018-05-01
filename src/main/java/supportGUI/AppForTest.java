c4c8049ef3486fbae4b2bc6f5afff2b3b68927b4
spark <spark@LP-SPARK-W10.hq.corp.viasat.com>
2018-05-01 12:22:42 -0600

    Initial Commit

---
+++ src/main/java/supportGUI/AppForTest.java
@@ -0,0 +1,12 @@
+package supportGUI;
+
+public class AppForTest {
+
+	public Boolean returnTrue(){
+		return true;
+	}
+	
+	public Boolean returnFalse(){
+		return false;
+	}
+}
---
+++ src/test/java/testCases/NewConnect.java
@@ -0,0 +1,39 @@
+package testCases;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewConnect {
+
+	@Test(priority=1)
+	public void J1170118(){
+		Assert.assertTrue(true);
+	}
+	
+	@Test(priority=2)
+	public void J1170119(){
+		Assert.assertTrue(true);
+	}
+	
+	@Test(priority=3)
+	public void J11701110(){
+		Assert.assertTrue(true);
+	}
+	
+	@Test(priority=4)
+	public void J11701111(){
+		Assert.assertTrue(true);
+	}
+	
+	@Test(priority=5)
+	public void J11701112(){
+		Assert.assertTrue(true);
+	}
+	
+	@Test(priority=6)
+	public void J11701113(){
+		Assert.assertTrue(true);
+	}
+}
